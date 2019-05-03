898b2a0a8dc387002e836358078c3d926bc25f4c
Mark Harder <markh@stratagen.local>
2019-05-02 18:26:17 -0700

    remove hidden files

--- src/helpers/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-  // Initialize Firebase
-  apiKey: 'AIzaSyB6VizL9026eUw8Zy-8E_B7ZmTSxC0hCzU',
-  authDomain: 'active-awards.firebaseapp.com',
-  databaseURL: 'https://active-awards.firebaseio.com',
-  projectId: 'active-awards',
-  storageBucket: 'active-awards.appspot.com',
-  messagingSenderId: '70096428256'
-}
